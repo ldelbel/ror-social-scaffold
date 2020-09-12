04931218e67dfa457854e6fa0f9c0a70251a5c27
ldelbel <lucdelbel@gmail.com>
2020-09-11 21:28:03 -0300

    Add requests specs for friendship and user

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+
+
+RSpec.describe 'Users', type: :request do
+  let(:signup) do
+    get '/users/sign_up'
+    post '/users', params: { user: { name: 'Lucas', email: 'Lucas@lucas.com', password: '123456', password_confirmation: '123456' } }
+  end
+
+  let(:signin) do
+    get '/users/sign_in'
+    post '/users', params: { user: {id: 1, email: 'lucas@lucas.com', password: '123456' } }
+  end
+
+  context 'when user tries to access root without signing in' do 
+    it 'redirects to signin page' do
+      get '/posts'
+      expect(response).to redirect_to('/users/sign_in')
+    end
+  end
+
+  context 'when user tries to access root signed in' do 
+    before do
+      DatabaseCleaner.clean
+      signup
+      signin
+    end
+    
+    it 'opens page' do
+      get '/posts'
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
