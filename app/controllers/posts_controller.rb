8a4bdd4ed9f013f52c0ecc3e3c4c1a03df851460
ldelbel <lucdelbel@gmail.com>
2020-09-16 12:03:27 -0300

    Change timeline_posts to show only from friends and self

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    @timeline_posts ||= Post.where('user_id IN (?) OR user_id IN (?)', current_user.friends.pluck(:id), [current_user.id]).ordered_by_most_recent.includes(:user)
   end
 
   def post_params
