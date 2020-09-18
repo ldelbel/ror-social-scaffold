5c322a1cf40f95d8a9a1fb99f10569d4fc61c05e
ldelbel <lucdelbel@gmail.com>
2020-09-17 22:10:52 -0300

    Change methods friends, pending_friends, friends_requests to model querys, refactor confirm_friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,29 +8,25 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, foreign_key: 'friend_id', class_name: 'Friendship'
+  
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
 
+  has_many :sent_requests, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :sent_requests, source: :friend
+
+  has_many :pending_requests, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :pending_requests, source: :user
+  
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  def friends
-    friends = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array = friends + inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
-    friends_array.compact
-  end
-
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
-  end
-
-  def confirm_friend(user)
-    friendship = inverse_friendships.find { |friend| friend.user == user }
+  def confirm_friend(friend)
+    friendship = Friendship.find_by( user_id: friend, friend_id: id )
     friendship.confirmed = true
     friendship.save
+    Friendship.create(user_id: id, friend_id: friend.id, confirmed: true)
   end
 
   def friend?(user)
